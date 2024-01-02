2c84ecf4d634b33a05417c5bfb55267a17409dd3
cheeksnpeeps <chmomar@gmail.com>
2024-01-02 10:00:37 -0500

    Simplify Security config and fix token validation Remove unnecessary logs Add logging to Security filters

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/authentication/SecurityConfiguration.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/authentication/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.cloudsuites.framework.webapp.authentication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,19 +26,14 @@ import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    @Value("${auth0.audience}")
-    private String audience;
-
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private String jwkSetUri;
 
     @Value("${web.cors.allowed-origins}")
     private String corsAllowedOrigins;
@@ -44,39 +41,62 @@ public class SecurityConfiguration {
     @Value("${spring.websecurity.debug:false}")
     boolean webSecurityDebug;
 
-    @Value("${okta.oauth2.groupsClaim:https://api.cloudsuites.com/roles}")
+    @Value("${okta.oauth2.groupsClaim}")
     private String claimGroups;
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        logger.debug("Configuring security filter chain with issuer: {}", issuer);
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(withDefaults())
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/**").permitAll()
+                                // Allow access to Swagger UI and API documentation endpoints
+                                .requestMatchers("/**","/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/webjars/**").permitAll()
+                                // Require authentication for any other request
                                 .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2ResourceServer ->
                         oauth2ResourceServer
                                 .jwt(jwt ->
                                         jwt
-                                                .decoder(JwtDecoders.fromOidcIssuerLocation(jwkSetUri))
-                                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                                                .decoder(JwtDecoders.fromOidcIssuerLocation(issuer))
                                 )
                 );
+        logger.debug("Security filter chain configured");
         return http.build();
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder
-                .withJwkSetUri("https://cheeksnpeeps.auth0.com/.well-known/jwks.json")
-                .build();
+    JwtDecoder jwtDecoder() {
+        logger.debug("Configuring JWT decoder with issuer: {}", issuer);
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+        jwtDecoder.setJwtValidator(withAudience);
+        logger.debug("JWT decoder configured");
+        return jwtDecoder;
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = getCorsConfiguration();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        logger.debug("CORS configuration source configured");
+        return source;
+    }
+
+    private CorsConfiguration getCorsConfiguration() {
+        logger.debug("Configuring CORS configuration source with allowed origins: {}", corsAllowedOrigins);
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList(corsAllowedOrigins));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
@@ -84,24 +104,26 @@ public class SecurityConfiguration {
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("X-Get-Header"));
         configuration.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        return configuration;
     }
 
     @Bean
     JwtAuthenticationConverter jwtAuthenticationConverter() {
+        logger.debug("Configuring JWT authentication converter with claim groups: {}", claimGroups);
         JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
         converter.setAuthoritiesClaimName(claimGroups);
         converter.setAuthorityPrefix("");
-
         JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
         jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+        logger.debug("JWT authentication converter configured");
         return jwtConverter;
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+        logger.debug("Configuring web security customizer: webSecurityDebug= {}", webSecurityDebug);
         return web -> web.debug(webSecurityDebug);
     }
+
+
 }
