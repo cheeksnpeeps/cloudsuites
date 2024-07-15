89281229b009347d9b0334221dfd38e94295f37f
cheeksnpeeps <chmomar@gmail.com>
2024-07-15 13:37:24 -0400

    Rename User module/service to Identity

--- services/identity-service/src/main/java/com/cloudsuites/framework/services/user/entities/Gender.java
+++ services/identity-service/src/main/java/com/cloudsuites/framework/services/user/entities/Gender.java
@@ -1,5 +1,5 @@
 package com.cloudsuites.framework.services.user.entities;
 
 public enum Gender {
-	MALE, FEMALE, UNKNOWN
+    MALE, FEMALE, UNKNOWN
 }
--- services/user-service/src/main/java/com/cloudsuites/framework/services/user/entities/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.cloudsuites.framework.services.user.entities;
-
-public enum Gender {
-	MALE, FEMALE, UNKNOWN
-}
