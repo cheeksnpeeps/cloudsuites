a2a98dda1ef3d1d2dbbb61ab9cba5bf3eae9a881
cheeksnpeeps <chmomar@gmail.com>
2024-07-16 23:24:42 -0400

    Switching to a unidirectional approach for the Tenant entity and using the buildingId directly

--- services/property-service/src/main/java/com/cloudsuites/framework/services/property/entities/Tenant.java
+++ services/property-service/src/main/java/com/cloudsuites/framework/services/property/entities/Tenant.java
@@ -2,24 +2,37 @@ package com.cloudsuites.framework.services.property.entities;
 
 import com.cloudsuites.framework.services.user.entities.Identity;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-@Data
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "tenant")
-public class Tenant{
+public class Tenant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "tenant_id")
     private Long tenantId;
 
+    @Column(name = "building_id")
+    private Long buildingId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ToString.Exclude
     private Identity identity;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "unit_id")
+    @ToString.Exclude
     private Unit unit;
 
     @ManyToOne
