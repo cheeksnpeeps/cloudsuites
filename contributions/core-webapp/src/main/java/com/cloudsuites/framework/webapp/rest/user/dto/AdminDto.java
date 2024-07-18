18f026ec876dddd980ace1dac7d81eec9a36ae80
cheeksnpeeps <chmomar@gmail.com>
2024-07-18 13:34:27 -0400

    Introduce Staffing Management

---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/AdminDto.java
@@ -0,0 +1,8 @@
+package com.cloudsuites.framework.webapp.rest.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AdminDto extends IdentityDto {
+}
---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/mapper/AdminMapper.java
@@ -0,0 +1,33 @@
+package com.cloudsuites.framework.webapp.rest.user.mapper;
+
+import com.cloudsuites.framework.webapp.rest.user.dto.AdminDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AdminMapper {
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public AdminMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public AdminDto convertToDTO(Admin admin) {
+        return modelMapper.map(admin, AdminDto.class);
+    }
+
+    public Admin convertToEntity(AdminDto adminDTO) {
+        return modelMapper.map(adminDTO, Admin.class);
+    }
+
+    public List<AdminDto> convertToDTOList(List<Admin> admins) {
+        return (admins != null && !admins.isEmpty()) ? admins.stream()
+                .map(this::convertToDTO)
+                .toList() : null;
+    }
+}
