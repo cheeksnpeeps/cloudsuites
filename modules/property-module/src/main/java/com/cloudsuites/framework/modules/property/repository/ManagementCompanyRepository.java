f4098a60010984925407801998d18a2dcca16aca
cheeksnpeeps <chmomar@gmail.com>
2024-07-19 17:02:38 -0400

    Remove back references and Buildings relationship from Management Company

--- modules/property-module/src/main/java/com/cloudsuites/framework/modules/property/repository/ManagementCompanyRepository.java
+++ modules/property-module/src/main/java/com/cloudsuites/framework/modules/property/repository/ManagementCompanyRepository.java
@@ -1,16 +1,11 @@
 package com.cloudsuites.framework.modules.property.repository;
 
 import com.cloudsuites.framework.services.property.entities.ManagementCompany;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ManagementCompanyRepository extends JpaRepository<ManagementCompany, Long> {
-    // Add custom query methods if needed
-    @EntityGraph(attributePaths = "buildings")
-    Optional<ManagementCompany> findById(Long id);
+
 }
 
