404d176353a787b4ac1e291071f1628922a6f7d2
cheeksnpeeps <chmomar@gmail.com>
2024-07-20 23:48:23 -0400

    Update Tenant entity with Building and Unit objects in Tenant Controller Save method

--- modules/property-module/src/main/java/com/cloudsuites/framework/modules/property/repository/TenantRepository.java
+++ modules/property-module/src/main/java/com/cloudsuites/framework/modules/property/repository/TenantRepository.java
@@ -11,11 +11,11 @@ import java.util.Optional;
 public interface TenantRepository extends JpaRepository<Tenant, Long> {
 
 
-    Optional<List<Tenant>> findByBuildingId(Long buildingId);
+    Optional<List<Tenant>> findByBuilding_BuildingId(Long buildingId);
 
-    Optional<List<Tenant>> findByBuildingIdAndUnit_UnitId(Long buildingId, Long unitId);
+    Optional<List<Tenant>> findByBuilding_BuildingIdAndUnit_UnitId(Long buildingId, Long unitId);
 
-    Optional<Tenant> findByBuildingIdAndUnit_UnitIdAndTenantId(Long buildingId, Long unitId, Long tenantId);
+    Optional<Tenant> findByBuilding_BuildingIdAndUnit_UnitIdAndTenantId(Long buildingId, Long unitId, Long tenantId);
 
     Optional<Tenant> findByIdentity_UserId(Long userId);
 }
