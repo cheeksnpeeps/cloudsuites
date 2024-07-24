2ae9e6c83138ca938c08a50294996b9062ee1965
cheeksnpeeps <chmomar@gmail.com>
2024-07-23 22:30:40 -0400

    Remove Save Unit and only rely on floor API

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/property/UnitRestController.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/property/UnitRestController.java
@@ -16,7 +16,6 @@ import io.swagger.v3.oas.annotations.tags.Tags;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,52 +36,18 @@ public class UnitRestController {
         this.unitMapper = unitMapper;
     }
 
-    @Operation(summary = "Get All Units by Floor", description = "Retrieve all units for a floor based on building ID and floor ID")
+    @Operation(summary = "Get All Units for a Building", description = "Retrieve all units for a specific building")
     @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json"))
-    @ApiResponse(responseCode = "404", description = "Building or floor not found")
+    @ApiResponse(responseCode = "404", description = "Building not found")
     @JsonView(Views.UnitView.class)
-    @GetMapping("/floors/{floorId}/units")
-    public ResponseEntity<List<UnitDto>> getAllUnitsByFloor(
-            @Parameter(description = "ID of the building") @PathVariable String buildingId,
-            @Parameter(description = "ID of the floor") @PathVariable String floorId)
-            throws NotFoundResponseException {
-        logger.debug("Getting all units for building: {} and floor: {}", buildingId, floorId);
-        List<UnitDto> units = unitMapper.convertToDTOList(unitService.getAllUnitsByFloor(buildingId, floorId));
-        logger.debug("Found {} units for building: {} and floor: {}", units.size(), buildingId, floorId);
-        return ResponseEntity.ok().body(units);
+    @GetMapping("/units")
+    public ResponseEntity<List<UnitDto>> getAllUnits(
+            @Parameter(description = "ID of the building") @PathVariable String buildingId) {
+        logger.debug("Getting all units for building: {}", buildingId);
+        List<Unit> units = unitService.getAllUnits(buildingId);
+        logger.debug("Found {} units for building: {}", units.size(), buildingId);
+        return ResponseEntity.ok().body(unitMapper.convertToDTOList(units));
     }
-
-
-    @Operation(summary = "Save a Unit", description = "Create a new unit for a floor based on building ID and floor ID")
-    @ApiResponse(responseCode = "201", description = "Unit created successfully", content = @Content(mediaType = "application/json"))
-    @ApiResponse(responseCode = "400", description = "Bad Request")
-    @ApiResponse(responseCode = "404", description = "Building or floor not found")
-    @JsonView(Views.UnitView.class)
-    @PostMapping("/floors/{floorId}/units")
-    public ResponseEntity<UnitDto> saveUnit(
-            @Parameter(description = "ID of the building") @PathVariable String buildingId,
-            @Parameter(description = "ID of the floor") @PathVariable String floorId,
-            @RequestBody @Parameter(description = "Unit details to be saved") UnitDto unitDTO) {
-        logger.debug("Saving unit for building: {} and floor: {}", buildingId, floorId);
-        UnitDto unit = unitMapper.convertToDTO(unitService.saveUnit(buildingId, floorId, unitMapper.convertToEntity(unitDTO)));
-        logger.debug("Unit saved successfully for building: {} and floor: {}", buildingId, floorId);
-        return ResponseEntity.status(HttpStatus.CREATED).body(unit);
-    }
-
-
-    @Operation(summary = "Delete a Unit by ID", description = "Delete a unit based on building ID and unit ID")
-    @ApiResponse(responseCode = "204", description = "Unit deleted successfully")
-    @ApiResponse(responseCode = "404", description = "Building or unit not found")
-    @DeleteMapping("/units/{unitId}")
-    public ResponseEntity<Void> deleteUnitById(
-            @Parameter(description = "ID of the building") @PathVariable String buildingId,
-            @Parameter(description = "ID of the unit to be deleted") @PathVariable String unitId) {
-        logger.debug("Deleting unit for building: {} and unit: {}", buildingId, unitId);
-        unitService.deleteUnitById(buildingId, "", unitId);
-        logger.debug("Unit deleted successfully for building: {} and unit: {}", buildingId, unitId);
-        return ResponseEntity.noContent().build();
-    }
-
 
     @Operation(summary = "Get a Unit by ID", description = "Retrieve unit details based on building ID and unit ID")
     @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json"))
@@ -100,17 +65,17 @@ public class UnitRestController {
         return ResponseEntity.ok().body(unitMapper.convertToDTO(unit));
     }
 
-    @Operation(summary = "Get All Units for a Building", description = "Retrieve all units for a specific building")
-    @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json"))
-    @ApiResponse(responseCode = "404", description = "Building not found")
-    @JsonView(Views.UnitView.class)
-    @GetMapping("/units")
-    public ResponseEntity<List<UnitDto>> getAllUnits(
-            @Parameter(description = "ID of the building") @PathVariable String buildingId) {
-        logger.debug("Getting all units for building: {}", buildingId);
-        List<Unit> units = unitService.getAllUnits(buildingId);
-        logger.debug("Found {} units for building: {}", units.size(), buildingId);
-        return ResponseEntity.ok().body(unitMapper.convertToDTOList(units));
+    @Operation(summary = "Delete a Unit by ID", description = "Delete a unit based on building ID and unit ID")
+    @ApiResponse(responseCode = "204", description = "Unit deleted successfully")
+    @ApiResponse(responseCode = "404", description = "Building or unit not found")
+    @DeleteMapping("/units/{unitId}")
+    public ResponseEntity<Void> deleteUnitById(
+            @Parameter(description = "ID of the building") @PathVariable String buildingId,
+            @Parameter(description = "ID of the unit to be deleted") @PathVariable String unitId) {
+        logger.debug("Deleting unit for building: {} and unit: {}", buildingId, unitId);
+        unitService.deleteUnitById(buildingId, "", unitId);
+        logger.debug("Unit deleted successfully for building: {} and unit: {}", buildingId, unitId);
+        return ResponseEntity.noContent().build();
     }
 
 }
