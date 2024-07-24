300afdf32c97989ac14427230c8306a29e157fcb
cheeksnpeeps <chmomar@gmail.com>
2024-07-23 23:54:32 -0400

    Fix Global Handler issue swallowing log stacktrace

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/GlobalExceptionHandler.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import com.cloudsuites.framework.services.common.exception.NotFoundResponseExcep
 import com.cloudsuites.framework.services.common.exception.ProblemDetails;
 import com.cloudsuites.framework.services.common.exception.ValidationException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,8 +19,12 @@ import java.time.ZonedDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ProblemDetails> handleCustomException(CustomException ex, HttpServletRequest request) {
+        logger.error("CustomException occurred: URI={}, Message={}", request.getRequestURI(), ex.getMessage(), ex);
+
         ProblemDetails problemDetails = ProblemDetails.builder()
                 .withTitle("Custom Error")
                 .withStatus(HttpStatus.BAD_REQUEST.value())
@@ -32,6 +38,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundResponseException.class)
     protected ResponseEntity<ProblemDetails> handleNotFoundResponseException(NotFoundResponseException ex, HttpServletRequest request) {
+        logger.error("NotFoundResponseException occurred: URI={}, Message={}", request.getRequestURI(), ex.getMessage(), ex);
         ProblemDetails problemDetails = ProblemDetails.builder()
                 .withTitle("Resource Not Found")
                 .withStatus(HttpStatus.NOT_FOUND.value())
@@ -45,6 +52,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ProblemDetails> handleAllExceptions(Exception ex, HttpServletRequest request) {
+        logger.error("Exception occurred: URI={}, Message={}", request.getRequestURI(), ex.getMessage(), ex);
         ProblemDetails problemDetails = ProblemDetails.builder()
                 .withTitle("Internal Server Error")
                 .withStatus(HttpStatus.INTERNAL_SERVER_ERROR.value())
@@ -58,6 +66,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ProblemDetails> handleValidationException(ValidationException ex, HttpServletRequest request) {
+        logger.error("ValidationException occurred: URI={}, Message={}", request.getRequestURI(), ex.getMessage(), ex);
         ProblemDetails problemDetails = ProblemDetails.builder()
                 .withTitle("Validation Error")
                 .withStatus(HttpStatus.BAD_REQUEST.value())
@@ -68,7 +77,4 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetails);
     }
-
-
-
 }
