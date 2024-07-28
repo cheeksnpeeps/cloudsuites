25e6f20c692057efbf5577c975d5e2b0ce636170
cheeksnpeeps <chmomar@gmail.com>
2024-07-27 20:37:35 -0400

    Add Test setup for OwnerRestController

--- contributions/core-webapp/src/test/java/com/cloudsuites/framework/webapp/rest/user/OwnerRestControllerTest.java
+++ contributions/core-webapp/src/test/java/com/cloudsuites/framework/webapp/rest/user/OwnerRestControllerTest.java
@@ -1,190 +1,117 @@
 package com.cloudsuites.framework.webapp.rest.user;
 
-import com.cloudsuites.framework.services.common.exception.NotFoundResponseException;
+import com.cloudsuites.framework.modules.property.features.repository.BuildingRepository;
+import com.cloudsuites.framework.modules.property.features.repository.FloorRepository;
+import com.cloudsuites.framework.modules.property.features.repository.UnitRepository;
+import com.cloudsuites.framework.modules.property.personas.repository.OwnerRepository;
+import com.cloudsuites.framework.services.property.features.entities.Building;
+import com.cloudsuites.framework.services.property.features.entities.Floor;
+import com.cloudsuites.framework.services.property.features.entities.Unit;
 import com.cloudsuites.framework.services.property.personas.entities.Owner;
-import com.cloudsuites.framework.services.property.personas.service.OwnerService;
+import com.cloudsuites.framework.services.user.entities.Address;
 import com.cloudsuites.framework.services.user.entities.Identity;
-import com.cloudsuites.framework.webapp.CloudsuitesCoreApplication;
-import com.cloudsuites.framework.webapp.GlobalExceptionHandler;
-import com.cloudsuites.framework.webapp.rest.user.dto.IdentityDto;
-import com.cloudsuites.framework.webapp.rest.user.dto.OwnerDto;
-import com.cloudsuites.framework.webapp.rest.user.mapper.OwnerMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test") // Use the test profile to load application-test.yml
+@Transactional // Rollback after each test
+public class OwnerRestControllerTest {
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.openMocks;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@SpringBootTest(classes = CloudsuitesCoreApplication.class)
-@ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.yml")
-class OwnerRestControllerTest {
-
+    String validOwnerId1;
+    String validOwnerId2;
+    String invalidOwnerId = "invalidOwnerId";
+    String invalidUnitId = "invalidUnitId";
+    @Autowired
     private MockMvc mockMvc;
-    @Mock
-    private OwnerService ownerService;
-    @Mock
-    private OwnerMapper mapper;
-    @InjectMocks
-    private OwnerRestController ownerRestController;
-    @Mock
+    @Autowired
+    private OwnerRepository ownerRepository;
+    @Autowired
     private ObjectMapper objectMapper;
-    @MockBean
-    private GlobalExceptionHandler globalExceptionHandler;
+    @Autowired
+    private BuildingRepository buildingRepository;
+    @Autowired
+    private UnitRepository unitRepository;
+    private String validUnitId1;
+    private String validUnitId2;
+    private String validBuildingId1;
+    private String validBuildingId2;
+    @Autowired
+    private FloorRepository floorRepository;
 
     @BeforeEach
     void setUp() {
-        openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(ownerRestController).build();
-        objectMapper = new ObjectMapper();
+        clearDatabase();
+
+        Identity identity1 = createIdentity("test1");
+        Identity identity2 = createIdentity("test2");
+
+        Owner owner1 = createOwner(identity1);
+        Owner owner2 = createOwner(identity2);
+
+        Building building1 = createBuilding("building1", "city1");
+        Building building2 = createBuilding("building2", "city2");
+
+        Unit unit1 = createUnit(building1, owner1);
+        Unit unit2 = createUnit(building2, owner2);
+
+        owner1.addUnit(unit1);
+        owner2.addUnit(unit2);
+
+        // Save owners, buildings, and units
+        ownerRepository.save(owner1);
+        ownerRepository.save(owner2);
+
+        this.validBuildingId1 = building1.getBuildingId();
+        this.validBuildingId2 = building2.getBuildingId();
+        this.validUnitId1 = unit1.getUnitId();
+        this.validUnitId2 = unit2.getUnitId();
+        this.validOwnerId1 = owner1.getOwnerId();
+        this.validOwnerId2 = owner2.getOwnerId();
     }
 
-    @Test
-    void testGetAllOwners_Success() throws Exception {
-        String ownerId = "OW-01J3M43433R0TR0ZXR8M4ZDC9T";
-        String ownerId2 = "OW-01J3M43433R0TR0ZXR8M4ZDC9T";
-        Owner owner = new Owner(); // create a sample owner object
-        owner.setOwnerId("OW-01J3M43433R0TR0ZXR8M4ZDC9T");
-        Owner owner2 = new Owner(); // create a sample owner object
-        owner2.setOwnerId("OW-01J3M43433R0TR0ZXR8M4ZDC9U");
-        List<Owner> owners = List.of(owner, owner2);
-        OwnerDto ownerDto = new OwnerDto(); // create a sample owner DTO
-        ownerDto.setOwnerId(ownerId);
-        OwnerDto ownerDto2 = new OwnerDto(); // create a sample owner DTO
-        ownerDto2.setOwnerId(ownerId2);
-        when(ownerService.getAllOwners()).thenReturn(owners);
-        when(mapper.convertToDTOList(owners)).thenReturn(List.of(ownerDto, ownerDto2));
 
-        mockMvc.perform(get("/api/v1/owners"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].ownerId").value(ownerId))
-                .andExpect(jsonPath("$[1].ownerId").value(ownerId2))
-                .toString();
-        verify(ownerService).getAllOwners();
-        verify(mapper).convertToDTOList(owners);
+    private void clearDatabase() {
+        ownerRepository.deleteAll();
+        buildingRepository.deleteAll();
+        unitRepository.deleteAll(); // Ensure units are also cleared
     }
 
-    @Test
-    void testGetOwnerById_Success() throws Exception {
-        String ownerId = "OW-01J3M43433R0TR0ZXR8M4ZDC9T";
-        Owner owner = new Owner(); // create a sample owner object
-        owner.setOwnerId(ownerId);
-        OwnerDto ownerDto = new OwnerDto(); // create a sample owner DTO
-        ownerDto.setOwnerId(ownerId);
-        when(ownerService.getOwnerById(ownerId)).thenReturn(owner);
-        when(mapper.convertToDTO(owner)).thenReturn(ownerDto);
-
-        mockMvc.perform(get("/api/v1/owners/{ownerId}", ownerId))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.ownerId").value(ownerId));
-
-        verify(ownerService).getOwnerById(ownerId);
-        verify(mapper).convertToDTO(owner);
-    }
-
-    @Test
-    void testGetOwnerById_NotFound() throws Exception {
-        String ownerId = "1";
-        when(ownerService.getOwnerById(ownerId)).thenThrow(new NotFoundResponseException("Owner not found"));
-        mockMvc.perform(get("/api/v1/owners/{ownerId}", ownerId))
-                .andExpect(status().isNotFound());
-        verify(ownerService).getOwnerById(ownerId);
-    }
-
-    @Test
-    void testUpdateOwner_Success() throws Exception {
-        String ownerId = "OW-01J3M43433R0TR0ZXR8M4ZDC9T";
-        OwnerDto ownerDto = new OwnerDto(); // create a sample owner DTO
-        ownerDto.setOwnerId(ownerId);
-        IdentityDto identityDto = new IdentityDto();
-        identityDto.setUsername("chmomar");
-        ownerDto.setIdentity(identityDto);
-        Owner owner = new Owner(); // create a sample owner object
-        owner.setOwnerId(ownerId);
+    private Identity createIdentity(String username) {
         Identity identity = new Identity();
-        identity.setUsername("chmomar");
+        identity.setUsername(username);
+        return identity; // Return the created identity
+    }
+
+    private Owner createOwner(Identity identity) {
+        Owner owner = new Owner();
         owner.setIdentity(identity);
-        when(mapper.convertToEntity(ownerDto)).thenReturn(owner);
-        when(ownerService.updateOwner(eq(ownerId), any(Owner.class))).thenReturn(owner);
-        when(mapper.convertToDTO(owner)).thenReturn(ownerDto);
-
-        mockMvc.perform(put("/api/v1/owners/{ownerId}", ownerId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(ownerDto)))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.ownerId").value(ownerId))
-                .andExpect(jsonPath("$.identity.username").value("chmomar"));
-        verify(ownerService).updateOwner(eq(ownerId), any(Owner.class));
-        verify(mapper).convertToDTO(owner);
+        return ownerRepository.save(owner); // Save and return the owner
     }
 
-    @Test
-    void testUpdateOwner_NotFound() throws Exception {
-        String ownerId = "1";
-        OwnerDto ownerDto = new OwnerDto(); // create a sample owner DTO
-        when(mapper.convertToEntity(ownerDto)).thenReturn(new Owner());
-        when(ownerService.updateOwner(eq(ownerId), any(Owner.class))).thenThrow(new NotFoundResponseException("Owner not found"));
-
-        mockMvc.perform(put("/api/v1/owners/{id}", ownerId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(ownerDto)))
-                .andExpect(status().isNotFound());
-
-        verify(ownerService).updateOwner(eq(ownerId), any(Owner.class));
+    private Building createBuilding(String name, String city) {
+        Building building = new Building();
+        building.setName(name);
+        Address address = new Address();
+        address.setCity(city);
+        building.setAddress(address);
+        return buildingRepository.save(building); // Save and return the building
     }
 
-    @Test
-    void getAllOwners() {
-        OwnerDto ownerDto = new OwnerDto();
-        ownerDto.setOwnerId("OW-01J3M43433R0TR0ZXR8M4ZDC9T");
-        OwnerDto ownerDto2 = new OwnerDto();
-        ownerDto2.setOwnerId("OW-013433R0TR0ZXR8M4ZDC9TWWDW");
-        List<OwnerDto> ownerDtos = List.of(ownerDto, ownerDto2);
-        try {
-            when(ownerService.getAllOwners()).thenReturn(List.of(new Owner(), new Owner()));
-        } catch (NotFoundResponseException e) {
-            e.printStackTrace();
-        }
-        when(mapper.convertToDTOList(any())).thenReturn(ownerDtos);
-
-    }
-
-    @Test
-    void getOwnerById() {
-    }
-
-    @Test
-    void updateOwner() {
-    }
-
-    @Test
-    void deleteOwner() {
-    }
-
-    @Test
-    void addUnitToOwner() {
-    }
-
-    @Test
-    void removeUnitFromOwner() {
+    private Unit createUnit(Building building, Owner owner) {
+        Unit unit = new Unit();
+        Floor floor = new Floor();
+        floor.setFloorName("floor");
+        unit.setFloor(floorRepository.save(floor));
+        unit.setBuilding(building);
+        //unit.setOwner(owner);
+        return unitRepository.save(unit); // Save and return the unit
     }
 }
