6b5a1f9b138f00e1b762c9ce17923feba27c89cc
cheeksnpeeps <chmomar@gmail.com>
2024-07-28 22:50:27 -0400

    Fix validation constraints in IdentityDto - Correct firstName and lastName validation constraints to match the required length

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/IdentityDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/IdentityDto.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,11 +21,12 @@ import lombok.NoArgsConstructor;
 public class IdentityDto {
 
     @Schema(hidden = true)
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     private String userId;
 
     @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Username of the user", example = "johndoe")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters long")
     private String username;
 
     @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
@@ -31,18 +35,23 @@ public class IdentityDto {
 
     @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "First name of the user", example = "John")
+    @Size(min = 2, max = 20, message = "First name must be between 3 and 20 characters long")
     private String firstName;
 
     @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Last name of the user", example = "Doe")
+    @Size(min = 2, max = 20, message = "Last name must be between 3 and 20 characters long")
     private String lastName;
 
     @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Phone number of the user", example = "+1234567890")
+    @Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 characters long")
+    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$", message = "Phone number must be in a valid format")
     private String phoneNumber;
 
     @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Email of the user", example = "johndoe@xyz.com")
+    @Size(min = 5, max = 50, message = "Email must be between 5 and 50 characters long")
+    @Email(message = "Email should be valid")
     private String email;
-
 }
