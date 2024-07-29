c7ea71c7d198a7aa3c500f6e062eaa406813ce3a
cheeksnpeeps <chmomar@gmail.com>
2024-07-28 23:46:09 -0400

    Refactor GlobalExceptionHandler to improve exception handling and logging - Introduce UsernameAlreadyExistsException - Set Username as unique

---
+++ services/common-service/src/main/java/com/cloudsuites/framework/services/common/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.cloudsuites.framework.services.common.exception;
+
+public class UsernameAlreadyExistsException extends Exception {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
