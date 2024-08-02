e94aa42045234cf1b332a312ffe500aa480d0fc6
cheeksnpeeps <chmomar@gmail.com>
2024-08-02 12:19:13 -0400

    Remove unecessary ManagerDTO, Staff is already taking care of it

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/ManagerDto.java
+++
@@ -1,7 +0,0 @@
-package com.cloudsuites.framework.webapp.rest.user.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ManagerDto extends IdentityDto {
-}
