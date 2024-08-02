880f7872744c88143452af1c248e1dfcb23c73ec
cheeksnpeeps <chmomar@gmail.com>
2024-08-02 12:14:27 -0400

    Validate username and add checks before updates and deletion

--- services/identity-service/src/main/java/com/cloudsuites/framework/services/user/AdminService.java
+++ services/identity-service/src/main/java/com/cloudsuites/framework/services/user/AdminService.java
@@ -1,27 +1,38 @@
 package com.cloudsuites.framework.services.user;
 
+import com.cloudsuites.framework.services.common.exception.InvalidOperationException;
 import com.cloudsuites.framework.services.common.exception.NotFoundResponseException;
+import com.cloudsuites.framework.services.common.exception.UsernameAlreadyExistsException;
 import com.cloudsuites.framework.services.user.entities.Admin;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public interface AdminService {
 
+    @Transactional
     Admin getAdminById(String adminId) throws NotFoundResponseException;
 
-    Admin createAdmin(Admin admin);
+    @Transactional
+    Admin createAdmin(Admin admin) throws UsernameAlreadyExistsException, InvalidOperationException;
 
+    @Transactional
     Admin updateAdmin(String adminId, Admin admin) throws NotFoundResponseException;
 
+    @Transactional
     void deleteAdmin(String adminId) throws NotFoundResponseException;
 
+    @Transactional
     List<Admin> getAllAdmins() throws NotFoundResponseException;
 
+    @Transactional
     Admin findByEmail(String email) throws NotFoundResponseException;
 
+    @Transactional
     Admin findByName(String email) throws NotFoundResponseException;
 
+    @Transactional
     Admin findByUserId(String userId) throws NotFoundResponseException;
 }
