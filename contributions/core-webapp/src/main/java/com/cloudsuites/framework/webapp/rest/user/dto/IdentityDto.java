0add092ee00046bb97ee14c0edde5fbc23d50e96
cheeksnpeeps <chmomar@gmail.com>
2024-08-02 12:17:40 -0400

    Adjust IdentityDto Visibility in API responses

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/IdentityDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/IdentityDto.java
@@ -22,36 +22,36 @@ import lombok.NoArgsConstructor;
 public class IdentityDto {
 
     @Schema(hidden = true)
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.AdminView.class, Views.OwnerView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     private String userId;
 
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.AdminView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Username of the user", example = "johndoe")
     @NotNull(message = "Username is required")
     @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters long")
     private String username;
 
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.AdminView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Gender of the user", example = "FEMALE")
     private Gender gender;
 
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.AdminView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "First name of the user", example = "John")
     @Size(min = 2, max = 20, message = "First name must be between 3 and 20 characters long")
     private String firstName;
 
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.AdminView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Last name of the user", example = "Doe")
     @Size(min = 2, max = 20, message = "Last name must be between 3 and 20 characters long")
     private String lastName;
 
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.AdminView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Phone number of the user", example = "+1234567890")
     @Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 characters long")
     @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$", message = "Phone number must be in a valid format")
     private String phoneNumber;
 
-    @JsonView({Views.StaffView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
+    @JsonView({Views.StaffView.class, Views.AdminView.class, Views.OwnerView.class, Views.TenantView.class, Views.UnitView.class})
     @Schema(description = "Email of the user", example = "johndoe@xyz.com")
     @Size(min = 5, max = 50, message = "Email must be between 5 and 50 characters long")
     @Email(message = "Email should be valid")
