724c860bc9b841d54197b5e5e88b0de0f1134752
cheeksnpeeps <chmomar@gmail.com>
2024-08-02 12:13:13 -0400

    Define Admin DTO

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/AdminDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/AdminDto.java
@@ -1,8 +1,39 @@
 package com.cloudsuites.framework.webapp.rest.user.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.cloudsuites.framework.services.user.entities.AdminRole;
+import com.cloudsuites.framework.services.user.entities.AdminStatus;
+import com.cloudsuites.framework.webapp.rest.property.dto.Views;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class AdminDto extends IdentityDto {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AdminDto {
+
+    @JsonView({Views.AdminView.class})
+    @Schema(hidden = true)
+    private String adminId;
+
+    @JsonView({Views.AdminView.class})
+    @Schema(description = "Admin identity details")
+    @Valid // Ensure that the Identity object is validated
+    @NotNull(message = "Identity is required")
+    private IdentityDto identity;
+
+    @JsonView(Views.AdminView.class)
+    @Schema(description = "Admin status", example = "ACTIVE")
+    private AdminStatus status;
+
+    @JsonView(Views.AdminView.class)
+    @Schema(description = "Role of the admin", example = "SUPER_ADMIN")
+    private AdminRole adminRole;
 }
