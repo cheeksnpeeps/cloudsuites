104ce9ccdd5b8a9288991c13c04ee5d0b93478c0
cheeksnpeeps <chmomar@gmail.com>
2024-08-03 21:38:07 -0400

    Define the security scheme for JWT

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/OpenApiConfig.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/OpenApiConfig.java
@@ -1,17 +1,34 @@
 package com.cloudsuites.framework.webapp;
 
 import com.cloudsuites.framework.webapp.rest.ContextFilter;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "Cloud Suites Framework", version = "1.0", description = "Cloud Suites Framework API Documentation"))
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER,
+        description = "Authorization header using the Bearer scheme. Example: \"Authorization: Bearer <token>\""
+)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("JWT");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Cloud Suites Framework")
@@ -21,7 +38,8 @@ public class OpenApiConfig {
                                 .email("chmomar@gmail.com")
                                 .name("Cheikh Lo")
                         )
-                );
+                )
+                .addSecurityItem(securityRequirement);
     }
 
     @Bean
