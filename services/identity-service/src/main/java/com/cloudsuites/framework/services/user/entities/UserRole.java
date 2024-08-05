72cca2e198ba6a6cce0b7233b94a24a3626c79ef
cheeksnpeeps <chmomar@gmail.com>
2024-08-04 23:19:37 -0400

    Create User role table for RBAC management. Added method to find user roles by identity, persona, and user type.

---
+++ services/identity-service/src/main/java/com/cloudsuites/framework/services/user/entities/UserRole.java
@@ -0,0 +1,33 @@
+package com.cloudsuites.framework.services.user.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_roles", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"identity_id", "persona_id", "user_type", "role"})
+})
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Surrogate primary key
+    @Column(name = "user_role_id")
+    private Long userRoleId; // Surrogate key for uniqueness
+
+    @ManyToOne
+    @JoinColumn(name = "identity_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+    private Identity identity; // Reference to the Identity table
+
+    @Column(name = "persona_id", nullable = false)
+    private String personaId; // The ID of the persona
+
+    @Enumerated(EnumType.STRING) // Using Enum for user types
+    @Column(name = "user_type", nullable = false)
+    private UserType userType; // Enum for user types (e.g., TENANT, OWNER, etc.)
+
+    @Column(name = "role", nullable = false)
+    private String role; // e.g., "ROLE_USER", "ROLE_ADMIN", etc.
+}
