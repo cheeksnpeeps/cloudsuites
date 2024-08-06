73e3c2dbe0628da5b478069243046778840566c0
cheeksnpeeps <chmomar@gmail.com>
2024-08-05 23:53:25 -0400

    Validate Claims based on User types and roles

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/authentication/util/JwtTokenHelper.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/authentication/util/JwtTokenHelper.java
@@ -28,9 +28,25 @@ public class JwtTokenHelper {
 
     public boolean validateTokenClaims(Claims claims, String buildingId, String unitId, String personaId) {
         String ownerIdClaim = claims.get(WebAppConstants.Claim.PERSONA_ID, String.class);
-        String buildingIdClaim = claims.get(WebAppConstants.Claim.BUILDING_ID, String.class);
-        String unitIdClaim = claims.get(WebAppConstants.Claim.UNIT_ID, String.class);
-        return ownerIdClaim.equals(personaId) && buildingIdClaim.equals(buildingId) && unitIdClaim.equals(unitId);
+        if (!personaId.equals(ownerIdClaim)) {
+            return false;
+        }
+        if (claims.get(WebAppConstants.Claim.TYPE, UserType.class) == UserType.ADMIN) {
+            return true;
+        } else if (claims.get(WebAppConstants.Claim.TYPE, UserType.class) == UserType.TENANT
+                || claims.get(WebAppConstants.Claim.TYPE, UserType.class) == UserType.OWNER) {
+            String buildingIdClaim = claims.get(WebAppConstants.Claim.BUILDING_ID, String.class);
+            String unitIdClaim = claims.get(WebAppConstants.Claim.UNIT_ID, String.class);
+            return buildingIdClaim.equals(buildingId) && unitIdClaim.equals(unitId);
+        } else if (claims.get(WebAppConstants.Claim.TYPE, UserType.class) == UserType.STAFF) {
+            if (buildingId != null) {
+                String buildingIdClaim = claims.get(WebAppConstants.Claim.BUILDING_ID, String.class);
+                return buildingIdClaim.equals(buildingId);
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 
     private JwtBuilder createClaims(String personaId, String buildingId, String unitId, String userId, UserType personaType) {
@@ -42,5 +58,28 @@ public class JwtTokenHelper {
                 .claim(WebAppConstants.Claim.BUILDING_ID, buildingId)
                 .claim(WebAppConstants.Claim.UNIT_ID, unitId)
                 .claim(WebAppConstants.Claim.USER_ID, userId);
+    }
+
+    public String generateToken(String adminId, UserType userType, String userId) {
+        JwtBuilder claims = createClaims(adminId, userId, userType);
+        return jwtTokenProvider.generateToken(claims);
+    }
+
+    private JwtBuilder createClaims(String adminId, String userId, UserType userType) {
+        return Jwts.builder()
+                .subject(adminId)
+                .audience().add(WebAppConstants.Claim.AUDIENCE)
+                .and()
+                .claim(WebAppConstants.Claim.TYPE, userType)
+                .claim(WebAppConstants.Claim.USER_ID, userId);
+    }
+
+    public String generateRefreshToken(String adminId, UserType userType, String userId) {
+        JwtBuilder claims = createClaims(adminId, userId, userType);
+        return jwtTokenProvider.generateRefreshToken(claims);
+    }
+
+    public boolean validateTokenClaims(Claims claims, String adminId) {
+        return validateTokenClaims(claims, null, null, adminId);
     }
 }
