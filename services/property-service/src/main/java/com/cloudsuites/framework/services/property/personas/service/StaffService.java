680b464e03ec88caa7281ba750f0fcdb09f49041
cheeksnpeeps <chmomar@gmail.com>
2024-08-05 23:56:20 -0400

    Make all services for staff Transactional

--- services/property-service/src/main/java/com/cloudsuites/framework/services/property/personas/service/StaffService.java
+++ services/property-service/src/main/java/com/cloudsuites/framework/services/property/personas/service/StaffService.java
@@ -10,18 +10,28 @@ import java.util.List;
 
 public interface StaffService {
 
+    @Transactional(readOnly = true)
     Staff getStaffById(String staffId) throws NotFoundResponseException;
 
+    @Transactional
     Staff updateStaff(String staffId, Staff staff) throws NotFoundResponseException;
 
+    @Transactional(readOnly = true)
     List<Staff> getAllStaffsByBuilding(String buildingId) throws NotFoundResponseException;
 
-    Staff createStaff(Staff staff) throws NotFoundResponseException, UsernameAlreadyExistsException, InvalidOperationException;
+    @Transactional
+    Staff createStaff(Staff staff, String companyId, String buildingId) throws NotFoundResponseException, UsernameAlreadyExistsException, InvalidOperationException;
 
+    @Transactional(readOnly = true)
     Staff findByUserId(String userId) throws NotFoundResponseException;
 
     @Transactional(readOnly = true)
     List<Staff> getAllStaffByCompany(String companyId) throws NotFoundResponseException;
 
+    @Transactional
     void deleteStaff(String staffId) throws NotFoundResponseException;
+
+    @Transactional(readOnly = true)
+    boolean hasAccessToBuilding(String username, String buildingID) throws NotFoundResponseException;
+
 }
