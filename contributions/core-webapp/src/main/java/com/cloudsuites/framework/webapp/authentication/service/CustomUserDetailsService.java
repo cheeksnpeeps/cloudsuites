190528ed3b53924ca242224baa3a93c2d2d6d6e5
cheeksnpeeps <chmomar@gmail.com>
2024-08-05 23:52:54 -0400

    Simplify User Details Service

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/authentication/service/CustomUserDetailsService.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/authentication/service/CustomUserDetailsService.java
@@ -1,29 +1,28 @@
 package com.cloudsuites.framework.webapp.authentication.service;
 
+import com.cloudsuites.framework.modules.user.UserRoleRepository;
 import com.cloudsuites.framework.services.common.exception.NotFoundResponseException;
 import com.cloudsuites.framework.services.user.UserService;
 import com.cloudsuites.framework.services.user.entities.Identity;
-import com.cloudsuites.framework.webapp.authentication.UserPrincipal;
+import com.cloudsuites.framework.services.user.entities.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserService userService;
+    private final UserRoleRepository userRoleRepository;
 
     @Autowired
-    public CustomUserDetailsService(UserService userService) {
+    public CustomUserDetailsService(UserService userService, UserRoleRepository userRoleRepository) {
         this.userService = userService;
+        this.userRoleRepository = userRoleRepository;
     }
 
     @Override
@@ -34,9 +33,8 @@ public class CustomUserDetailsService implements UserDetailsService {
         } catch (NotFoundResponseException e) {
             throw new UsernameNotFoundException("Identity not found: " + username);
         }
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        Map<String, Long> personaIds = new HashMap<>();
-        return UserPrincipal.create(identity, personaIds, authorities);
+        List<UserRole> roles = userRoleRepository.findUserRoleByIdentityId(identity.getUserId());
+        return new CustomUserDetails(identity, roles);
     }
 
     private Identity findUser(String username) throws NotFoundResponseException {
