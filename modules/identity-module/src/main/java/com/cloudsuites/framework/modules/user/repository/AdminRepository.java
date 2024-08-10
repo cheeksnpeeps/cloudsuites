723a087240e7c96902cd7ee3571622d0900c3ed8
cheeksnpeeps <chmomar@gmail.com>
2024-08-09 22:44:48 -0400

    Add JPA queries to fetch admins by Role and Status

--- modules/identity-module/src/main/java/com/cloudsuites/framework/modules/user/repository/AdminRepository.java
+++ modules/identity-module/src/main/java/com/cloudsuites/framework/modules/user/repository/AdminRepository.java
@@ -1,9 +1,12 @@
-package com.cloudsuites.framework.modules.user;
+package com.cloudsuites.framework.modules.user.repository;
 
 import com.cloudsuites.framework.services.user.entities.Admin;
+import com.cloudsuites.framework.services.user.entities.AdminRole;
+import com.cloudsuites.framework.services.user.entities.AdminStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +17,8 @@ public interface AdminRepository extends JpaRepository<Admin, String> {
     Optional<Admin> findByIdentity_UserId(String userId);
 
     Optional<Admin> findByIdentity_Email(String email);
+
+    List<Admin> findByRole(AdminRole adminRole);
+
+    List<Admin> findByRoleAndStatus(AdminRole adminRole, AdminStatus status);
 }
