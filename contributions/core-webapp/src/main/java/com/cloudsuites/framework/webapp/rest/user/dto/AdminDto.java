112e460786115df9c44dc47733b1d03f92061d2a
cheeksnpeeps <chmomar@gmail.com>
2024-08-09 22:44:05 -0400

    Add Admin fields to Admin Role APIs

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/AdminDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/user/dto/AdminDto.java
@@ -19,22 +19,22 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AdminDto {
 
-    @JsonView({Views.AdminView.class})
+    @JsonView({Views.AdminView.class, Views.AdminRoleView.class})
     @Schema(hidden = true)
     private String adminId;
 
-    @JsonView({Views.AdminView.class})
+    @JsonView({Views.AdminView.class, Views.AdminRoleView.class})
     @Schema(description = "Admin identity details")
     @Valid // Ensure that the Identity object is validated
     @NotNull(message = "Identity is required")
     private IdentityDto identity;
 
-    @JsonView(Views.AdminView.class)
+    @JsonView({Views.AdminRoleView.class, Views.AdminView.class})
     @Schema(description = "Admin status", example = "ACTIVE")
     @NotNull(message = "Status is required")
     private AdminStatus status;
 
-    @JsonView(Views.AdminView.class)
+    @JsonView({Views.AdminRoleView.class, Views.AdminView.class})
     @Schema(description = "Role of the admin", example = "SUPER_ADMIN")
     @NotNull(message = "Role is required")
     private AdminRole role;
