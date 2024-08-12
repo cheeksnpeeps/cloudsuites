138b456e37494929e351911a4bca9a121338f904
cheeksnpeeps <chmomar@gmail.com>
2024-08-12 00:04:24 -0400

    Fix package structure

--- modules/notification-module/src/main/java/com/cloudsuites/framework/modules/notification/config/FirebaseConfig.java
+++ modules/notification-module/src/main/java/com/cloudsuites/framework/modules/notification/config/FirebaseConfig.java
@@ -1,4 +1,4 @@
-package com.cloudsuites.framework.modules.notification;
+package com.cloudsuites.framework.modules.notification.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
