a16b5e942892abef8c61b04854b52dd91faeeea3
cheeksnpeeps <chmomar@gmail.com>
2024-08-12 23:20:14 -0400

    Add lease repository an Status

---
+++ modules/property-module/src/main/java/com/cloudsuites/framework/modules/property/features/repository/LeaseRepository.java
@@ -0,0 +1,9 @@
+package com.cloudsuites.framework.modules.property.features.repository;
+
+import com.cloudsuites.framework.services.property.features.entities.Lease;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeaseRepository extends JpaRepository<Lease, String> {
+
+    Lease findByTenantIdAndUnitIdAndOwnerId(String tenantId, String unitId, String ownerId);
+}
