d910736ba5c8c4b49b217e26625be14bb945e423
cheeksnpeeps <chmomar@gmail.com>
2024-08-13 15:54:45 -0400

    Push SMS and Email support scaffolding

--- modules/notification-module/src/main/java/com/cloudsuites/framework/modules/notification/model/EmailNotificationRequest.java
+++ modules/notification-module/src/main/java/com/cloudsuites/framework/modules/notification/model/EmailNotificationRequest.java
@@ -1,4 +1,13 @@
 package com.cloudsuites.framework.modules.notification.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class EmailNotificationRequest {
+
+    private String to;
+    private String subject;
+    private String body;
 }
