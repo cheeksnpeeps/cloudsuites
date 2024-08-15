d56264d6e2dcacabca17ce29f367f577f5622f8d
cheeksnpeeps <chmomar@gmail.com>
2024-08-14 23:31:41 -0400

    Validate Identity by Email in place of Usernames during registration

--- modules/identity-module/src/main/java/com/cloudsuites/framework/modules/user/repository/UserRepository.java
+++ modules/identity-module/src/main/java/com/cloudsuites/framework/modules/user/repository/UserRepository.java
@@ -3,17 +3,20 @@ package com.cloudsuites.framework.modules.user.repository;
 import com.cloudsuites.framework.services.user.entities.Identity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Identity, String> {
+
+    @Transactional(readOnly = true)
     Optional<Identity> findByPhoneNumber(String phoneNumber);
 
+    @Transactional(readOnly = true)
     Optional<Identity> findByEmail(String email);
 
-    boolean existsByUsername(String username);
-
-    Optional<Identity> findByUsername(String username);
+    @Transactional(readOnly = true)
+    boolean existsByEmail(String email);
 }
 
