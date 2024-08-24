e7873eba7e22c1db530638ee180cf99acf1d2cb7
cheeksnpeeps <chmomar@gmail.com>
2024-08-23 23:16:40 -0400

    Remove unecessary custom query

--- modules/property-module/src/main/java/com/cloudsuites/framework/modules/property/features/repository/LeaseRepository.java
+++ modules/property-module/src/main/java/com/cloudsuites/framework/modules/property/features/repository/LeaseRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeaseRepository extends JpaRepository<Lease, String> {
 
-    Lease findByTenantIdAndUnitIdAndOwnerId(String tenantId, String unitId, String ownerId);
-
     Lease findByUnitIdAndOwnerId(String ownerId, String unitId);
 }
