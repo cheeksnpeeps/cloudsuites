0cc556ff8637433b874e61b4bd378886be37237c
cheeksnpeeps <chmomar@gmail.com>
2024-08-27 21:44:37 -0400

    Proper implementation of amenity booking

--- services/amenity-service/src/main/java/com/cloudsuites/framework/services/amenity/service/AmenityBookingService.java
+++ services/amenity-service/src/main/java/com/cloudsuites/framework/services/amenity/service/AmenityBookingService.java
@@ -1,16 +1,20 @@
 package com.cloudsuites.framework.services.amenity.service;
 
-import com.cloudsuites.framework.services.amenity.entities.Amenity;
 import com.cloudsuites.framework.services.amenity.entities.booking.AmenityBooking;
 import com.cloudsuites.framework.services.amenity.entities.booking.BookingException;
+import org.springframework.scheduling.annotation.Async;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 
 public interface AmenityBookingService {
+
+    @Async
+    CompletableFuture<AmenityBooking> asyncBookAmenity(String amenityId, String userId, LocalDateTime startTime, LocalDateTime endTime) throws BookingException;
 
     AmenityBooking bookAmenity(String amenityId, String userId, LocalDateTime startTime, LocalDateTime endTime) throws BookingException;
 
     void cancelBooking(String bookingId) throws BookingException;
 
-    boolean isAvailable(Amenity amenity, LocalDateTime startTime, LocalDateTime endTime);
+    boolean isAvailable(String amenityId, LocalDateTime startTime, LocalDateTime endTime);
 }
