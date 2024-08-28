17bf68f49002bb0ef607eb5ed2afd95b61d75374
cheeksnpeeps <chmomar@gmail.com>
2024-08-27 21:46:16 -0400

    Add building limit period and allow Async operations

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/CloudsuitesCoreApplication.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/CloudsuitesCoreApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = "com.cloudsuites.framework")
 @ComponentScan({"com.cloudsuites.framework.modules", "com.cloudsuites.framework.services", "com.cloudsuites.framework.webapp"})
 @EnableJpaRepositories({"com.cloudsuites.framework.modules"})
 @EntityScan(basePackages = {"com.cloudsuites.framework.services"})
+@EnableAsync
 public class CloudsuitesCoreApplication {
 
 	public static void main(String[] args) {
