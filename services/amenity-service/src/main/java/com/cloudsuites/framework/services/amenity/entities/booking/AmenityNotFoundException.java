a422363834f69f46d80235f7af8068643cb14df2
cheeksnpeeps <chmomar@gmail.com>
2024-08-29 11:32:05 -0400

    Add exception handling in case Amenity is not found

---
+++ services/amenity-service/src/main/java/com/cloudsuites/framework/services/amenity/entities/booking/AmenityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.cloudsuites.framework.services.amenity.entities.booking;
+
+public class AmenityNotFoundException extends RuntimeException {
+    public AmenityNotFoundException(String message) {
+        super(message);
+    }
+}
+
