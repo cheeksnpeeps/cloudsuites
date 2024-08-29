4c89ba5f32032d28191fe2de90eb70d4e613e6fc
cheeksnpeeps <chmomar@gmail.com>
2024-08-29 11:17:14 -0400

    Creating Amenity Building Repo

--- modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/repository/AmenityBuildingRepository.java
+++ modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/repository/AmenityBuildingRepository.java
@@ -3,5 +3,20 @@ package com.cloudsuites.framework.modules.amenity.repository;
 import com.cloudsuites.framework.services.amenity.entities.AmenityBuilding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface AmenityBuildingRepository extends JpaRepository<AmenityBuilding, String> {
+
+    List<AmenityBuilding> findByBuildingId(String buildingId);
+
+    List<AmenityBuilding> findByAmenityId(String amenityId);
+
+    void deleteByAmenityIdAndBuildingIdIn(String amenityId, Set<String> toRemove);
+
+    boolean existsByAmenityIdAndBuildingId(String amenityId, String buildingId);
+
+    void deleteByAmenityId(String amenityId);
+
+    List<AmenityBuilding> findByAmenityIdAndBuildingId(String amenityId, String buildingId);
 }
