81a43fe44497ef29310e541b9311ed4e6b6479b3
cheeksnpeeps <chmomar@gmail.com>
2024-09-01 00:15:29 -0400

    Fix unit tests for calendar service implementation

--- modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/AmenityBookingServiceImpl.java
+++ modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/AmenityBookingServiceImpl.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-@Service
+@Component
 public class AmenityBookingServiceImpl implements AmenityBookingService {
 
     private final AmenityRepository amenityRepository;
