186ed3f96285611cb79af21b1dc729b49dba52f7
cheeksnpeeps <chmomar@gmail.com>
2024-09-02 11:21:24 -0400

    Create AmenityBooking DTO and Mappings

---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/mapper/AmenityBookingMapper.java
@@ -0,0 +1,35 @@
+package com.cloudsuites.framework.webapp.rest.amenityBooking.mapper;
+
+import com.cloudsuites.framework.services.amenity.entities.booking.AmenityBooking;
+import com.cloudsuites.framework.webapp.rest.amenity.dto.AmenityBookingDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AmenityBookingMapper {
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public AmenityBookingMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public AmenityBooking convertToEntity(AmenityBookingDto amenityBookingDto) {
+        return modelMapper.map(amenityBookingDto, AmenityBooking.class);
+    }
+
+    public List<AmenityBookingDto> convertToDTOList(List<AmenityBooking> amenities) {
+        return amenities.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public AmenityBookingDto convertToDTO(AmenityBooking amenityBooking) {
+        return modelMapper.map(amenityBooking, AmenityBookingDto.class);
+    }
+}
