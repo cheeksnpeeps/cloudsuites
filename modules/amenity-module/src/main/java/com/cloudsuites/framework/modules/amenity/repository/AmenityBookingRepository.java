6ec599581671b95c1abe6511357c283bfbfda30d
cheeksnpeeps <chmomar@gmail.com>
2024-09-02 23:26:47 -0400

    Manage maximum Occupancy  during booking

--- modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/repository/AmenityBookingRepository.java
+++ modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/repository/AmenityBookingRepository.java
@@ -26,13 +26,15 @@ public interface AmenityBookingRepository extends JpaRepository<AmenityBooking, 
     int deleteByEndTimeBefore(LocalDateTime cutoffDate);
 
     @Transactional
-    @Query("SELECT b FROM AmenityBooking b WHERE b.userId = :userId AND (:type IS NULL OR b.amenity.type = :type) AND (:startDate IS NULL OR b.startTime >= :startDate) AND (:endDate IS NULL OR b.endTime <= :endDate)")
+    @Query("SELECT b FROM AmenityBooking b WHERE b.userId = :userId AND (:type IS NULL OR b.amenity.type = :type)" +
+            " AND (:startDate IS NULL OR b.startTime >= :startDate) AND (:endDate IS NULL OR b.endTime <= :endDate)")
     List<AmenityBooking> findByUserIdAndFilters(@Param("userId") String userId,
                                                 @Param("type") AmenityType type,
                                                 @Param("startDate") LocalDateTime startDate,
                                                 @Param("endDate") LocalDateTime endDate);
 
-    @Query("SELECT b FROM AmenityBooking b WHERE b.amenity.amenityId = :amenityId AND b.startTime >= :start AND b.endTime <= :end")
+    @Query("SELECT b FROM AmenityBooking b WHERE b.amenity.amenityId = :amenityId AND b.startTime >= :start " +
+            "AND b.endTime <= :end")
     List<AmenityBooking> findByAmenityIdAndTimeRange(@Param("amenityId") String amenityId,
                                                      @Param("start") LocalDateTime start,
                                                      @Param("end") LocalDateTime end);
@@ -40,5 +42,14 @@ public interface AmenityBookingRepository extends JpaRepository<AmenityBooking, 
 
     @Transactional
     List<AmenityBooking> findByAmenity_AmenityId(String amenityId);
+
+    @Query("SELECT COUNT(b) FROM AmenityBooking b WHERE b.amenity.amenityId = :amenityId " +
+            "AND b.startTime < :endTime AND b.endTime > :startTime")
+    int countOccupancyDuringTimeRange(@Param("amenityId") String amenityId,
+                                      @Param("startTime") LocalDateTime startTime,
+                                      @Param("endTime") LocalDateTime endTime);
+
+    @Transactional
+    List<AmenityBooking> findByAmenity_AmenityIdAndBookingId(String amenityId, String bookingId);
 }
 
