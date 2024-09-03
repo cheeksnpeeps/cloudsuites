9225fcda3097697038234a2667e1f683a777ef5a
cheeksnpeeps <chmomar@gmail.com>
2024-09-02 23:25:55 -0400

    Set default values for Amenity Booking details

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingDto.java
@@ -23,7 +23,9 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AmenityBookingDto {
 
-    private Long bookingId;
+    @Schema(hidden = true)
+    @JsonView({Views.AmenityBooking.class})
+    private String bookingId;
 
     @NotBlank
     @Schema(description = "Amenity ID")
