8a693666612d96c7df210ad92fa75bd0e0118a50
cheeksnpeeps <chmomar@gmail.com>
2024-09-02 23:30:13 -0400

    Propagate the security context to asynchronous tasks to allow asyncDispatch to piggy back on the request authorization

---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/AsyncConfig.java
@@ -0,0 +1,37 @@
+package com.cloudsuites.framework.webapp;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("Async-Executor-");
+        executor.initialize();
+        // Wrap executor with a DelegatingSecurityContextAsyncTaskExecutor to propagate security context
+        return new DelegatingSecurityContextAsyncTaskExecutor(executor);
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable, method, objects) -> {
+            // Handle uncaught exceptions for async methods
+            System.err.println("Async error in method: " + method.getName());
+            throwable.printStackTrace();
+        };
+    }
+
+}
