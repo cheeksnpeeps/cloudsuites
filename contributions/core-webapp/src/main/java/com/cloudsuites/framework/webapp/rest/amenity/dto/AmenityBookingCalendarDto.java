425b857fc0db1f17e96ce67508c5efd29960c76d
cheeksnpeeps <chmomar@gmail.com>
2024-09-03 22:42:13 -0400

    Create proper DTO for Booking Calendar

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingCalendarDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingCalendarDto.java
@@ -1,15 +1,33 @@
 package com.cloudsuites.framework.webapp.rest.amenity.dto;
 
-import com.cloudsuites.framework.services.amenity.entities.booking.AmenityBooking;
+import com.cloudsuites.framework.webapp.rest.property.dto.Views;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AmenityBookingCalendarDto {
 
+    @Schema(description = "Amenity ID")
+    @JsonView({Views.AmenityBooking.class})
     private String amenityId;
-    private String amenityName;
-    private String amenityType;
-    private List<AmenityBooking> bookedSlots;
+
+    @JsonView({Views.AmenityBooking.class})
+    @Schema(description = "Booked slots")
+    private List<AmenityBookingDto> bookedSlots;
+
+    @JsonView({Views.AmenityBooking.class})
+    @Schema(description = "Available slots")
     private List<LocalDateTime> availableSlots;
 }
