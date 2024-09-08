d5fd8599b339ac9ad0fd34d8982fca2471b9ad68
cheeksnpeeps <chmomar@gmail.com>
2024-09-07 22:52:09 -0400

    Create GolfSimulator amenity DTO and entity

---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/features/GolfSimulatorDto.java
@@ -0,0 +1,31 @@
+package com.cloudsuites.framework.webapp.rest.amenity.dto.features;
+
+import com.cloudsuites.framework.webapp.rest.amenity.dto.AmenityDto;
+import com.cloudsuites.framework.webapp.rest.property.dto.Views;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonTypeName("GOLF_SIMULATOR")
+public class GolfSimulatorDto extends AmenityDto {
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Brand or model of the golf simulator", example = "TrackMan")
+    private String simulatorModel;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if clubs are provided", example = "true")
+    private Boolean providesClubs;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Maximum number of players at a time", example = "4")
+    private Integer maxPlayers;
+}
