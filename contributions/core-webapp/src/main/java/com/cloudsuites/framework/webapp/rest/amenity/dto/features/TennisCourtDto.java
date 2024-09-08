0cc75de924dd81dd7d014613c887b5523ce54782
cheeksnpeeps <chmomar@gmail.com>
2024-09-08 14:53:46 -0400

    Create Theater amenity DTO and entity

---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/features/TennisCourtDto.java
@@ -0,0 +1,65 @@
+package com.cloudsuites.framework.webapp.rest.amenity.dto.features;
+
+import com.cloudsuites.framework.services.amenity.entities.AmenityType;
+import com.cloudsuites.framework.webapp.rest.amenity.dto.AmenityDto;
+import com.cloudsuites.framework.webapp.rest.property.dto.Views;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonTypeName("TENNIS_COURT")
+public class TennisCourtDto extends AmenityDto {
+
+    @JsonView({Views.AmenityView.class, Views.BuildingView.class})
+    @Schema(description = "Type of the amenity", example = "SWIMMING_POOL")
+    @NotNull(message = "Type is mandatory")
+    private AmenityType type;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Surface type of the tennis court (e.g., clay, hard, grass)", example = "Clay")
+    private String courtSurface;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if the court is indoor", example = "false")
+    private Boolean isIndoor;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if the court has lighting for night play", example = "true")
+    private Boolean lighting;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Height of the net in meters", example = "1.07")
+    private Double netHeight;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if there are seats for spectators", example = "true")
+    private Boolean hasSpectatorSeats;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if there are benches for players to sit", example = "true")
+    private Boolean hasBenches;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Fee for booking the tennis court", example = "30.00")
+    private BigDecimal bookingFee;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Size of the court in square meters", example = "650.0")
+    private Double courtSize;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if the court can be booked", example = "true")
+    private Boolean isBookable;
+}
+
--- services/amenity-service/src/main/java/com/cloudsuites/framework/services/amenity/entities/features/TennisCourt.java
+++ services/amenity-service/src/main/java/com/cloudsuites/framework/services/amenity/entities/features/TennisCourt.java
@@ -1,14 +1,44 @@
 package com.cloudsuites.framework.services.amenity.entities.features;
 
 import com.cloudsuites.framework.services.amenity.entities.Amenity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 @Entity
 @Table(name = "tennis_court")
 public class TennisCourt extends Amenity {
 
+    @Column(name = "court_surface")
+    private String courtSurface; // Surface type of the tennis court (e.g., clay, hard, grass)
+
+    @Column(name = "is_indoor")
+    private Boolean isIndoor; // Indicates if the court is indoor
+
+    @Column(name = "lighting")
+    private Boolean lighting; // Indicates if the court has lighting for night play
+
+    @Column(name = "net_height")
+    private Double netHeight; // Height of the net in meters
+
+    @Column(name = "has_spectator_seats")
+    private Boolean hasSpectatorSeats; // Indicates if there are seats for spectators
+
+    @Column(name = "has_benches")
+    private Boolean hasBenches; // Indicates if there are benches for players to sit
+
+    @Column(name = "booking_fee")
+    private BigDecimal bookingFee; // Fee for booking the tennis court
+
+    @Column(name = "court_size")
+    private Double courtSize; // Size of the court in square meters
+
+    @Column(name = "is_bookable")
+    private Boolean isBookable; // Indicates if the court can be booked
 }
+
 
