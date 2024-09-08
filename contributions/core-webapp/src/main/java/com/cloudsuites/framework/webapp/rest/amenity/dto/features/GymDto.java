625bac89b5e178b88cc095ce8170042826cba327
cheeksnpeeps <chmomar@gmail.com>
2024-09-07 22:52:36 -0400

    Create Gym amenity DTO and entity

---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/features/GymDto.java
@@ -0,0 +1,31 @@
+package com.cloudsuites.framework.webapp.rest.amenity.dto.features;
+
+import com.cloudsuites.framework.webapp.rest.amenity.dto.AmenityDto;
+import com.cloudsuites.framework.webapp.rest.property.dto.Views;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonTypeName("GYM")
+public class GymDto extends AmenityDto {
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Number of gym machines available", example = "30")
+    private Integer numberOfMachines;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if personal trainers are available", example = "true")
+    private Boolean hasPersonalTrainers;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if fitness classes are offered", example = "true")
+    private Boolean hasFitnessClasses;
+}
