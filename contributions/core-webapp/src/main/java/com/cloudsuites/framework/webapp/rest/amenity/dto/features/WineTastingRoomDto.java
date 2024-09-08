cfe55ac7bd0d269b4f2dc93106ee80387e93e72e
cheeksnpeeps <chmomar@gmail.com>
2024-09-08 14:53:18 -0400

    Create WineTastingRoom amenity DTO and entity

---
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/features/WineTastingRoomDto.java
@@ -0,0 +1,38 @@
+package com.cloudsuites.framework.webapp.rest.amenity.dto.features;
+
+import com.cloudsuites.framework.services.amenity.entities.AmenityType;
+import com.cloudsuites.framework.webapp.rest.amenity.dto.AmenityDto;
+import com.cloudsuites.framework.webapp.rest.property.dto.Views;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonTypeName("WINE_TASTING_ROOM")
+public class WineTastingRoomDto extends AmenityDto {
+
+    @JsonView({Views.AmenityView.class, Views.BuildingView.class})
+    @Schema(description = "Type of the amenity", example = "SWIMMING_POOL")
+    @NotNull(message = "Type is mandatory")
+    private AmenityType type;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Number of wines available for tasting", example = "50")
+    private Integer numberOfWinesAvailable;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Indicates if private events are allowed", example = "true")
+    private Boolean allowsPrivateEvents;
+
+    @JsonView(Views.AmenityView.class)
+    @Schema(description = "Max seating capacity of the wine tasting room", example = "20")
+    private Integer seatingCapacity;
+}
