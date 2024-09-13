09dfc2d5047a08751fa9dc1e8848be922fe34e37
cheeksnpeeps <chmomar@gmail.com>
2024-09-12 23:36:01 -0400

    Redesigning the Amenity Booking Calendar DTO to be more Tenant and Staff focused

--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingCalendarDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingCalendarDto.java
@@ -1,26 +1,30 @@
 package com.cloudsuites.framework.webapp.rest.amenity.dto;
 
-import com.cloudsuites.framework.services.property.personas.entities.Tenant;
+import com.cloudsuites.framework.webapp.rest.property.dto.Views;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonView(Views.BookingCalendarView.class)
 public class AmenityBookingCalendarDto {
 
-    @Schema(description = "Tenant details")
-    private Tenant tenant;
+    @Schema(hidden = true)
+    @JsonView(Views.BookingCalendarView.class)
+    private AmenityScheduleDto amenitySchedule;
 
-    @Schema(description = "List of amenities with their schedules and booked slots")
-    private List<AmenityScheduleDto> amenities;
-
+    @JsonView(Views.BookingCalendarView.class)
+    @Schema(description = "Filters for the calendar", example = "{\"startDate\":\"2021-08-01T00:00:00\",\"endDate\":\"2021-08-31T23:59:59\"," +
+            "\"byAmenityTypes\":[\"SWIMMING_POOL\",\"EVEVATOR\"]," +
+            "\"byBookingStatus\":[\"PENDING\",\"APPROVED\"]," +
+            "\"byAmenityIds\":[\"AMN-01J79E2Z8MQXSAVM1AMCFK2ADZ\",\"AMN-01J77YPYDY1CDAR0CSAJZ6GY9W\"]}")
+    private CalendarBookingFiltersDto filters;
 }
--- contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingDto.java
+++ contributions/core-webapp/src/main/java/com/cloudsuites/framework/webapp/rest/amenity/dto/AmenityBookingDto.java
@@ -4,6 +4,7 @@ import com.cloudsuites.framework.services.amenity.entities.booking.BookingStatus
 import com.cloudsuites.framework.webapp.rest.property.dto.Views;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.PrePersist;
@@ -14,6 +15,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -24,43 +26,50 @@ import java.time.LocalDateTime;
 public class AmenityBookingDto {
 
     @Schema(hidden = true)
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
     private String bookingId;
 
     @NotBlank
     @Schema(hidden = true)
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
     private String amenityId;
 
     @NotBlank
     @Schema(hidden = true)
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
+    @JsonProperty("createdBy")
     private String userId;
+
+    @Setter
+    @NotBlank
+    @Schema(hidden = true)
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
+    private Boolean isCurrentUser;
 
     @NotNull
     @FutureOrPresent
     @Schema(description = "Start time of the booking", example = "2021-12-23T08:00:00")
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
     private LocalDateTime startTime;
 
     @NotNull
     @Future
     @Schema(description = "End time of the booking", example = "2021-12-23T10:00:00")
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
     private LocalDateTime endTime;
 
     @Schema(hidden = true)
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
     private BookingStatus status;
 
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
     @Schema(hidden = true)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
     private LocalDateTime createdAt;
 
-    @JsonView({Views.AmenityBooking.class})
+    @JsonView({Views.AmenityBooking.class, Views.BookingCalendarView.class})
     @Schema(hidden = true)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
     private LocalDateTime updatedAt;
@@ -69,4 +78,5 @@ public class AmenityBookingDto {
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
+
 }
