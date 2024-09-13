2269c6680dd36d6736fe60fb417092f4e2e946f5
cheeksnpeeps <chmomar@gmail.com>
2024-09-12 23:49:43 -0400

    Ability to query by Amenity Types and IDs

--- modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/repository/AmenityRepository.java
+++ modules/amenity-module/src/main/java/com/cloudsuites/framework/modules/amenity/repository/AmenityRepository.java
@@ -1,6 +1,7 @@
 package com.cloudsuites.framework.modules.amenity.repository;
 
 import com.cloudsuites.framework.services.amenity.entities.Amenity;
+import com.cloudsuites.framework.services.amenity.entities.AmenityType;
 import jakarta.persistence.LockModeType;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,8 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AmenityRepository extends JpaRepository<Amenity, String> {
@@ -19,4 +22,8 @@ public interface AmenityRepository extends JpaRepository<Amenity, String> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT a FROM Amenity a WHERE a.amenityId = :amenityId")
     Amenity lockAmenityForBooking(@Param("amenityId") String amenityId);
+
+    @Transactional
+    List<Amenity> findByAmenityIdInAndTypeIn(List<String> amenityIds, List<AmenityType> types);
+
 }
